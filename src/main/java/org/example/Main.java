463d332de13dc22ab645d3dcc5e03ba09e9f4660
burotti1 <burotti1@gmail.com>
2022-11-03 09:15:02 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,10 +10,10 @@ public class Main {
         MediaType mediaType = MediaType.parse("application/soap+xml; charset=utf-8");
         RequestBody body = RequestBody.create(mediaType, "" +
                 "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\">\r\n  <soap12:Body>\r\n    <TCKimlikNoDogrula xmlns=\"http://tckimlik.nvi.gov.tr/WS\">\r\n      " +
-                "<TCKimlikNo>41056987280</TCKimlikNo>\r\n      " +
-                "<Ad>burak eren</Ad>\r\n      " +
-                "<Soyad>özdemir</Soyad>\r\n      " +
-                "<DogumYili>1998</DogumYili>\r\n    " +
+                "<TCKimlikNo>TCKimlikNo</TCKimlikNo>\r\n      " +
+                "<Ad>Ad</Ad>\r\n      " +
+                "<Soyad>Soyad</Soyad>\r\n      " +
+                "<DogumYili>DogumYili</DogumYili>\r\n    " +
                 "</TCKimlikNoDogrula>\r\n  " +
                 "</soap12:Body>\r\n</soap12:Envelope>");
         Request request = new Request.Builder()
